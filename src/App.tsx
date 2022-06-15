66b45486c7415983e40e7cf0b9e31e39fd969f3d
vitinhom <104538433+vitinhom@users.noreply.github.com>
2022-06-14 21:51:09 -0300

    incio cadastrar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Cabecalho from "./components/Cabecalhofeed";
 import './App.css';
 import CabecalhoSite from "./components/CabecalhoSite";
+import Cadastrar from "./components/Cadastrar";
 
 const App = function() {
   return (
    <div className="app">
       <CabecalhoSite/>
       <Cabecalho/>
+      <Cadastrar/>
      </div>
   );
 }
--- src/components/CabecalhoSite.tsx
+++ src/components/CabecalhoSite.tsx
@@ -1,16 +1,17 @@
-import './Cabecalho.css'
+
 import './CabecalhoSite.css'
+import imagemLupa from '../imagens/lupa.svg'
+import imagemLogo from '../imagens/imagemlogo.png'
 const CabecalhoSite = function(){
     return (
         <header className='CabecalhoSite'>
             <main className='CabecalhoSiteFrom'>
-                <div className='logo'>
-                    <img src="" alt="Logo" />
-                </div>
+                <img src={imagemLogo} className='logo' alt="Logo" />
+                
                 <div className='pesquisa'>
                     <input type="text" className='PesquisaCabecalho' name='Search' placeholder='Pesquisar' />
                     <a href="" className='botaoPesquisa'>
-                        <img src="" className='ImagemBotao' alt="Lupa"  />
+                        <img src={imagemLupa} className='ImagemBotao' alt="Lupa"  />
                     </a>
                 </div>
 
