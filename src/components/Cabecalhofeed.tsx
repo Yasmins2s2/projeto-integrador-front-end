419fd272bc429a35d8f70626a9950fd0ff043eea
Yasmin Alves <yasminadesouza@gmail.com>
2022-06-21 21:53:57 -0300

    continuação do cabeçalho

--- src/components/Cabecalhofeed.tsx
+++ src/components/Cabecalhofeed.tsx
@@ -10,7 +10,6 @@ const Cabecalho = function() {
           <a className='menu-item2' href="">Notificações</a>
           <a className='menu-item3' href="">Chat</a>
         </nav>
-        <div className=''></div>
       </header>
   
     )
