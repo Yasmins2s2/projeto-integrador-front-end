1ab03db5cd1ecf8f4fb67333c0c1454d2cfae405
NicolasSimao <105399462+NicolasSimao@users.noreply.github.com>
2022-06-27 20:12:53 -0300

    Adiciona a camada model

---
+++ src/models/Peca.ts
@@ -0,0 +1,14 @@
+enum TipoPeca {
+    Sobreposicao = 'SOBREPOSICAO',
+    ParteCima = 'PARTE_CIMA',
+    ParteBaixo = 'PARTE_BAIXO',
+}
+
+type Peca = {
+    id_peca:number
+    nome:string
+    url_imagem: string
+    tipo: TipoPeca
+}
+
+export default Peca
