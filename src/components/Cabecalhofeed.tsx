953849b9e4a4fca84a82aba0e560057d6e47630e
Yasmin Alves <yasminadesouza@gmail.com>
2022-06-27 21:26:53 -0300

    feed

--- src/components/Cabecalhofeed.tsx
+++ src/components/Cabecalhofeed.tsx
@@ -1,18 +1,55 @@
 import './Cabecalhofeed.css';
 import './cabecalho2' ;
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHeart, faSquarePlus, faUser, faComment, faBookmark} from '@fortawesome/free-regular-svg-icons';
+import {faLocationArrow, faHouseChimney, faMagnifyingGlass, faWandMagicSparkles} from '@fortawesome/free-solid-svg-icons';
+import imagem1 from "../imagens/roupa.jpg"
 
 const Cabecalho = function() {
     return (
-      <header className="cabecalhofeed">
-        <a  className='logotipo' href=""><img src="" alt="" /></a>
-        <nav className='menu'>
-          <a className='menu-item1' href="">Publicações</a>
-          <a className='menu-item2' href="">Notificações</a>
-          <a className='menu-item3' href="">Chat</a>
-        </nav>
-      </header>
-  
-    )
+      <>
+       <header className='CabecalhoFeed'> 
+          <div className='Logo'>
+            <h1>AppFell</h1>
+          </div>
+          <div className='icones'>
+            <FontAwesomeIcon icon={faHeart} />
+            <FontAwesomeIcon icon={faSquarePlus} />
+            <FontAwesomeIcon icon={faLocationArrow} />
+          </div>
+       </header>
+       <main>
+         <div className='border'>
+           <div className='conteudo'>
+            <div className='Foto1'>
+              <img src="" alt="" />
+                @LukacomK 
+                <i></i>
+                <i></i>
+                <i></i>
+            </div>
+            <div className='Foto1'>
+              <img src={imagem1} alt="" />
+                <FontAwesomeIcon icon={faHeart} />
+                <FontAwesomeIcon icon={faComment} />
+                <FontAwesomeIcon icon={faBookmark} />
+            </div>
+            
+
+           </div>
+         </div>
+       </main>
+       <footer>
+         Rodapé
+         <div className='icones2'>
+            <FontAwesomeIcon icon={faHouseChimney} />
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <FontAwesomeIcon icon={faWandMagicSparkles} />
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+       </footer>
+      </>
+    );
   }
   
   export default Cabecalho;
