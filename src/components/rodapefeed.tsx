2aa335fda523cd5124a04b5ed0cc34cff782bccc
vitinhom <104538433+vitinhom@users.noreply.github.com>
2022-07-05 21:13:36 -0300

    guarda-roupa arrumado

---
+++ src/components/rodapefeed.tsx
@@ -0,0 +1,11 @@
+const Rodape = function() {
+    return (
+        <footer>
+            <a className="container-1">Guarda-Roupa</a>
+            <a className="container-2"></a>
+        </footer>
+    );
+};
+
+
+export default Rodape;
